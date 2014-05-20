bc25a6894beee49f650f1015b3036f00b905bd82
Josiah Faulkner-Harrington <jfaulknerharrington6996@sohamcollege.org.uk>
2014-05-20 23:03:10 +0100

    Update diceRoller.py
    and updated version of the diceroller.py program including a user input and a option to keep rolling the dice until you roll and 6. this has been used with loops, functions and global variables

--- diceRoller.py
+++ diceRoller.py
@@ -1,7 +1,9 @@
-#Coin flip program
-#Describe the purpose of this program here.
+#edited by josiah
+#dice roll program
+#the purpose of this program is to roll the dice to display a number to the user and as a addition it will ask the user if they woule like to keep rolling the dice untill they get a 6, this is made using funtions.
 
-inport random;time
+import random
+import time
 
 s1 = "- - - - -\n|       |\n|   O   |\n|       |\n- - - - -\n"
 s2 = "- - - - -\n| O     |\n|       |\n|     O |\n- - - - -\n"
@@ -9,26 +11,65 @@ s3 = "- - - - -\n| O     |\n|   O   |\n|     O |\n- - - - -\n"
 s4 = "- - - - -\n| O   O |\n|       |\n| O   O |\n- - - - -\n"
 s5 = "- - - - -\n| O   O |\n|   O   |\n| O   O |\n- - - - -\n"
 s6 = "- - - - -\n| O   O |\n| O   O |\n| O   O |\n- - - - -\n"
-
+x = 0
 def roll():
-    Print("rolling....."
-    roll = rand.randing(7)
+    print("rolling.....")
+    roll = random.randint(1,6)
+    return roll
 
+    
 
-def show_dice(roll)
-    if roll = 1:
-        print(S1)
-    else roll = 2
+def show_dice(roll):
+    global x
+    if roll == 1:
+        print(s1)
+    elif roll == 2:
         print(s2)
-    else roll = 3
+    elif roll == 3:
         print(s3)
-    else roll = 4
+    elif roll == 4:
         print(s4)
-    else roll = 5
+    elif roll == 5:
         print(s5)
-    else roll = 6
+    else:
         print(s6)
+        x = x + 10
+    return myroll
+    return x
 
-roll
+User_Input = input("if you would like to roll the dice please type roll?")
+if User_Input == "roll":
+    myroll = roll()
+else:
+    print("this Program will now end")
+    quit
+
+    
+def Rolling6():
+    global x
+    ask6 = input("would you like to run this program until you get a 6?\n"
+                 "Y = Yes\n"
+                 "N = No\n")
+
+    if ask6 == "Y":
+        while x == 0 :
+            roll = random.randint(1,6)
+            time.sleep(0.5)
+            show_dice(roll)
+        if x == 10:
+            print("you have rolled a 6")
+            quit
+
+        else:
+            print("this Program will now end")
+            quit
+                
+    
+
+
 time.sleep(1)
-show_dice(roll)
+
+show_dice(myroll)
+
+Rolling6()
+
